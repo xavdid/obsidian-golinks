6eec0be40bd5d46778a1bb93773cc56aeda85b20
David Brownman <beamneocube@gmail.com>
2022-07-01 20:15:49 -0700

    basic implementation

--- main.ts
+++ main.ts
@@ -1,137 +1,68 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/* eslint-disable prefer-const */
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class GoLinksPlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownPostProcessor((element, context) => {
+      // console.log(element, context);
+      // console.log(element);
+      const elementsToCheck = element.querySelectorAll("p, li");
+      // console.log("found", textAndListItems);
+      elementsToCheck.forEach((element) => {
+        let changed = false;
+        const result: Node[] = [];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+        element.childNodes.forEach((node) => {
+          if (node.nodeType !== Node.TEXT_NODE) {
+            result.push(node);
+            return;
+          }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+          let text = node.textContent || "";
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+          let nextLink: ReturnType<string["match"]> = null;
+          do {
+            nextLink = text.match(/go\/[_\d\w-/]+/);
+            // todo: make function
+            if (nextLink === null) {
+              continue;
+            }
 
-	async onload() {
-		await this.loadSettings();
+            changed = true;
+            result.push(document.createTextNode(text.slice(0, nextLink.index)));
+            // <a aria-label-position="top" aria-label="http://go/actual-link" rel="noopener" class="external-link" href="http://go/actual-link" target="_blank">go/bad</a>
+            const newLink = document.createElement("a");
+            newLink.href = `http://${nextLink[0]}`;
+            newLink.text = nextLink[0];
+            result.push(newLink);
+            text = text.slice((nextLink.index ?? 0) + nextLink[0].length);
+          } while (nextLink != null);
+          result.push(document.createTextNode(text));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+          // https://www.golinks.com/help/golinks-naming-conventions/
+          // const goLinks = (node.textContent ?? "").match(
+          // 	/go\/[_\d\w-/]+/
+          // );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+          // for (const goLink of goLinks) {
+          // 	changed = true;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+          // 	console.log(goLink);
+          // }
+        });
+        if (changed) {
+          element.setChildrenInPlace(result);
+        }
+      });
+    });
+  }
 }
